8abbc8f67145bc86d0cabf846dbed9f6a1bb1fed
vinceyyy <vincenty17@icloud.com>
2021-01-25 16:44:56 -0500

    added looping

--- run.py
+++ run.py
@@ -1,13 +1,19 @@
 from manager.manager import Manager
 from cleaner.judge import negative_bool
 
-KEYWORDS = "h1b"
+KEYWORDS_LIST = ["google analytics", "chinese marketing", "chinese analyst"]
 
-indeed = Manager("indeed", KEYWORDS)
-indeed.build_index()
-indeed.keep_trying()
-print("===========================")
-print("FINISHED.")
-print(f"{len(indeed.df)} jobs retrived.")
-indeed.save()
-indeed.save_friendly()
+for keyword in [keyword for keyword in KEYWORDS_LIST]:
+    indeed = Manager("indeed", keyword)
+    if indeed.any_result() == True:
+        indeed.build_index()
+        indeed.keep_trying()
+    indeed.log_error()
+    print("===========================")
+    print("FINISHED.")
+    print(f"{len(indeed.df)} jobs retrived.")
+    indeed.save()
+    indeed.save_friendly()
+    print("============")
+    print(f"{keyword} FILE SAVED.")
+    print("============")
