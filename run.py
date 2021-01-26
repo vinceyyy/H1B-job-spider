6df17bf2c349d82565c0b1f7a6a2619429de22ce
vinceyyy <vincenty17@icloud.com>
2021-01-26 10:59:51 -0500

    added tqdm; changed output

--- run.py
+++ run.py
@@ -1,18 +1,26 @@
 from manager.manager import Manager
-from cleaner.judge import negative_bool
 
-KEYWORDS_LIST = ["google analytics", "chinese marketing", "chinese analyst"]
+KEYWORDS_LIST = [
+    "growth marketing",
+    "marketing analyst",
+    "multicultural marketing",
+    "google analytics",
+    "chinese marketing",
+    "chinese analyst",
+]
 
 for keyword in [keyword for keyword in KEYWORDS_LIST]:
     indeed = Manager("indeed", keyword)
     if indeed.any_result() == True:
         indeed.build_index()
         indeed.keep_trying()
+        indeed.pbar.close()
     indeed.log_error()
     print("===========================")
     print("FINISHED.")
     print(f"{len(indeed.df)} jobs retrived.")
-    indeed.save()
+    indeed.save_raw()
+    indeed.save_neutral()
     indeed.save_friendly()
     print("============")
     print(f"{keyword} FILE SAVED.")
